f9c3e847e41ba015556bfdb8a6a070c02e08c8e2
dabrattoli <davidbrattoli@yahoo.com>
2016-11-13 23:01:56 -0500

    Updated category template

--- category.php
+++ category.php
@@ -9,5 +9,48 @@
 
 get_header(); ?>
 
+<div class="row page-content">
+<div class="small-12 medium-12 large-12 columns breadcrumb-container">
+   <?php get_template_part( 'template-parts/content', 'breadcrumb' ); ?>
+</div>
+<div class="medium-4 large-4 columns hide-for-small-only">
+	<div class="small-12 medium-12 large-12 columns sidebar-widget">
+		<div class="small-12 medium-12 large-12 columns sidebar-menu-header">
+	<h3><?php echo bloginfo('the-title'); ?></h3>
+		</div>
+	<?php	if ( has_nav_menu( 'left-nav' ) ) : ?>
+	<div id="secondary" class="secondary">
+		<?php if ( has_nav_menu( 'left-nav' ) ) : ?>
+			<nav id="site-navigation" class="main-navigation" role="navigation">
+				<?php
+					// Primary navigation menu.
+					wp_nav_menu( array(
+						'menu_class'     => 'nav-menu',
+						'theme_location' => 'left-nav',
+					) );
+				?>
+			</nav><!-- .main-navigation -->
+				<?php endif; ?>
+		</div>
+		<?php endif; ?>
+
+	</div>
+	</div>
+	<div class="small-12 medium-8 large-8 columns">		
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+           <header class="page-header">
+               <?php
+                    echo '<h1 class="page-title">'.single_cat_title( '', false ).'</h1>';
+					the_archive_description( '<div class="taxonomy-description">', '</div>' );
+				?>
+            </header><!-- .page-header -->    
+            
+		</main><!-- #main -->
+	</div><!-- #primary -->
+</div>	
+
+</div>
 
 <?php get_footer(); ?>
