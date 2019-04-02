05b1d85704f257e1f36f06d22ab1abf1ef5eede0
Joe <joecue@gmail.com>
2019-04-02 11:49:08 -0400

    Fixed Menu ID for removing aria attribute

--- js/menu-cleanup.js
+++ js/menu-cleanup.js
@@ -5,7 +5,7 @@ jQuery(document).ready(function( $ ){
 	
 	$('ul.nav-menu li').removeAttr('aria-haspopup');
 	$('ul.nav-menu').removeAttr('aria-expanded');	
-    $('#menu-navigation').removeAttr('aria-expanded');
+    $('#menu-primary-menu').removeAttr('aria-expanded');
     $('ul.nav-menu li').removeAttr('aria-expanded');
     
 });
